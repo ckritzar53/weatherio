aebbacd9ac73c912ceeb80d7da767bd48dd3ad72
ckritzar53 <ckritzar53@busarmydude.org>
2025-07-17 12:50:57 -0400

    Update module.js

--- assest/js/module.js
+++ assest/js/module.js
@@ -2,7 +2,7 @@
 
 // Corrected: Array now starts with Sunday to match the getUTCDay() output.
 export const weekDayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-export const monthNames = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+export const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export const getDate = (dateUnix, timezone) => {
     const date = new Date((dateUnix + timezone) * 1000);
@@ -26,7 +26,7 @@ export const getTime = (timeUnix, timezone, timeFormat = '12h') => {
 
 export const aqiText = {
     1: { level: "Good", message: "Air quality is considered satisfactory, and air pollution poses little or no risk." },
-    2: { level: "Fair", message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution." },
+    2: { level: "Fair", message: "Air quality is acceptable; however, for some pollutants, there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution." },
     3: { level: "Moderate", message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected." },
     4: { level: "Poor", message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects." },
     5: { level: "Very Poor", message: "Health warnings of emergency conditions. The entire population is more likely to be affected." }
